cce01766e58b651d1e10c87eece4a55482e2f01a
mason <29055903@qq.com>
2017-09-01 18:54:19 +0800

    test branch dev

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -60,7 +60,7 @@ class SiteController extends Controller
             ],
             'captcha' => [
                 'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testyou' : null,
             ],
         ];
     }
