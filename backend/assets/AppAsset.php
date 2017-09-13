8c50c779a36f8499bec8d38534d0175e221605ed
mason <29055903@qq.com>
2017-09-13 18:23:30 +0800

    init adminUI

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -3,6 +3,7 @@
 namespace backend\assets;
 
 use yii\web\AssetBundle;
+use yii\web\View;
 
 /**
  * Main backend application asset bundle.
@@ -12,12 +13,26 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+//        'css/site.css',
+        "inspinia/css/bootstrap.min.css",
+        "inspinia/font-awesome/css/font-awesome.css",
+//        "inspinia/css/plugins/codemirror/codemirror.css",
+//        "inspinia/css/plugins/codemirror/ambiance.css",
+        "inspinia/css/plugins/toastr/toastr.min.css",
+        "inspinia/js/plugins/gritter/jquery.gritter.css",
+        "inspinia/css/animate.css",
+        "inspinia/css/style.css",
     ];
+
+    public $jsOptions = [
+        'position' => View::POS_HEAD,
+    ];
+
     public $js = [
+        "inspinia/js/jquery-3.1.1.min.js",
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+//        'yii\web\YiiAsset',
+//        'yii\bootstrap\BootstrapAsset',
     ];
 }
