04225c1504626e72d76833316b50c22ce839d9a7
huochaitou <gufe198@163.com>
2017-12-23 13:33:21 +0800

    fixed a bug

--- backend/views/permission/list.php
+++ backend/views/permission/list.php
@@ -16,26 +16,6 @@ use yii\web\View;
 <?php AppAsset::addCssFile($this, "@web/inspinia/css/plugins/footable/footable.core.css"); ?>
 
 
-    <div class="row wrapper border-bottom white-bg page-heading">
-        <div class="col-lg-10">
-            <h2>FooTable</h2>
-            <ol class="breadcrumb">
-                <li>
-                    <a href="index.html">Home</a>
-                </li>
-                <li>
-                    <a>Tables</a>
-                </li>
-                <li class="active">
-                    <strong>FooTable</strong>
-                </li>
-            </ol>
-        </div>
-        <div class="col-lg-2">
-
-        </div>
-    </div>
-
     <div class="wrapper wrapper-content animated fadeInRight">
         <!--
         <div class="row">
--- backend/views/project/list.php
+++ backend/views/project/list.php
@@ -15,27 +15,6 @@ use yii\web\View;
     <!--<link href="css/plugins/footable/footable.core.css" rel="stylesheet">-->
 <?php AppAsset::addCssFile($this, "@web/inspinia/css/plugins/footable/footable.core.css"); ?>
 
-
-    <div class="row wrapper border-bottom white-bg page-heading">
-        <div class="col-lg-10">
-            <h2>FooTable</h2>
-            <ol class="breadcrumb">
-                <li>
-                    <a href="index.html">Home</a>
-                </li>
-                <li>
-                    <a>Tables</a>
-                </li>
-                <li class="active">
-                    <strong>FooTable</strong>
-                </li>
-            </ol>
-        </div>
-        <div class="col-lg-2">
-
-        </div>
-    </div>
-
     <div class="wrapper wrapper-content animated fadeInRight">
         <!--
         <div class="row">
